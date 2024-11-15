5b864712a9bceefc714df0d4374f1e7102dfbde5
evie-8 <neouma29@gmail.com>
2024-11-15 09:47:25 +0300

    Remove .docusaurus from tracking

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\HP\\Desktop\\sunbird\\sunbird-docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\HP\\Desktop\\sunbird\\sunbird-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\HP\\Desktop\\sunbird\\sunbird-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\HP\\Desktop\\sunbird\\sunbird-docs\\src\\css\\custom.css"),
-];
