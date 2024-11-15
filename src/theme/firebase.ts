328e044fd1d93dacdbf70e768893c2c77273d7c5
evie-8 <neouma29@gmail.com>
2024-11-15 11:19:32 +0300

    adding environment variables to workflow file

--- src/theme/firebase.ts
+++ src/theme/firebase.ts
@@ -7,7 +7,9 @@ const app = firebase.initializeApp({
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID, // if you have this
+  appId: process.env.FIREBASE_APP_ID,
 });
 
 export const auth = getAuth(app);
