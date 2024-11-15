8002b05c5eb1491b25e769ddc1bd6a5a48e1acf1
evie-8 <neouma29@gmail.com>
2024-11-15 10:36:05 +0300

    solving issue: changing base url to /sunbird-docs/

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://sunbird-docs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/sunbird-docs/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
